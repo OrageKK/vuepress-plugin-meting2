0736c98ba73882e4e978520812f84c3cd9bed006
oragekk <oragekk@163.com>
2024-02-19 17:25:55 +0800

    publish v1.4.0 适配vuepress-theme-hope 2.0.0-rc.24

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,12 +1,12 @@
-import { defineClientConfig } from "@vuepress/client";
+import { ClientConfig } from "@vuepress/client";
 import MetingGlobal from "./components/MetingGlobal/index.js";
 import Meting from "./components/Meting/index.js";
 import APlayer from "./components/APlayer/index.js";
-
+declare const defineClientConfig: (clientConfig?: ClientConfig) => ClientConfig;
 export default defineClientConfig({
   async enhance({ app }) {
     app.component("APlayer", APlayer);
     app.component("Meting", Meting);
   },
   rootComponents: [MetingGlobal],
-});
+}) as ClientConfig;
